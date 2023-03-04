730e1498af4aa629cf41f4861724b0c7771fdd1f
kaique-smaug <108313417+kaique-smaug@users.noreply.github.com>
2023-03-04 15:18:13 -0300

    Add files via upload

---
+++ api.js
@@ -0,0 +1,41 @@
+// Adicionando menu Hamburguer
+class mobileNavbar {
+    constructor(mobileMenu, navList, navLinks) {
+        this.mobileMenu = document.querySelector(mobileMenu);
+        this.navList = document.querySelector(navList);
+        this.navLinks = document.querySelectorAll(navLinks);
+        this.activeClass = "active";
+        this.handleClick =  this.handleClick.bind(this);
+    };
+
+    animateLinks() {
+        this.navLinks.forEach((link, index) => {
+            console.log(index / 7 + 0.3);
+              link.style.animation  
+              ? (link.style.animation = "")
+              : (link.style.animation = 'navLinkFade 0.2s ease forwards 0.3s')
+        });
+    };
+
+    handleClick() {
+        this.navList.classList.toggle(this.activeClass);
+        this.mobileMenu.classList.toggle(this.activeClass);
+        this.animateLinks();
+    };
+    addClickEvent() {
+        this.mobileMenu.addEventListener("click", this.handleClick);
+    };
+    init() {
+        if (this.mobileMenu) {
+            this.addClickEvent();
+        };
+        return this;
+    };
+};
+
+mobileNavbar = new mobileNavbar(
+    '.mobile-menu',
+    '.nav-list',
+    '.nav-list li',
+);
+mobileNavbar.init();
